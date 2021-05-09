647ee5b7cf38e9a063177cd87eaf3b1b2e31585e
ISIRA_PC <it19017334@my.sliit.lk>
2021-05-09 17:58:38 +0530

    New changes to master

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,23 @@
-import axios from "axios";
+import axios from'axios'; //Going to use to make api calls
 
-const url = 'http://localhost:5000/posts';
+const API = axios.create({baseURL:'http://localhost:5000'})
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost)
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+
+    return req;
+});
+
+//const url = 'https://memories-project-kasuni.herokuapp.com/posts';
+//const url = 'http://localhost:5000/posts';
+
+export const fetchPosts = () => API.get('/posts');
+export const createPost = (newPost) => API.post('/posts', newPost);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+
+export const signIn = (formData) => API.post('/user/signin', formData);
+export const signUp = (formData) => API.post('/user/signup', formData);
